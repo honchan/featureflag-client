4960aa0077e665b4c37ddb5d3f56bce2f6e2cc90
honchan <honkangchan@gmail.com>
2021-08-05 21:52:13 +0800

    update get featurerules to use react-query

---
+++ src/querys/useFeatureRules.tsx
@@ -0,0 +1,36 @@
+import { useQuery } from 'react-query'
+
+import { apiService } from '../api/api-service'
+import { FeatureRuleDefault, FeatureRuleOnetime, FeatureRuleWhitelist} from '../api/api-types'
+
+type Props = {
+  flagId: string | null
+}
+
+type QueryResult = {
+  default: FeatureRuleDefault
+  onetime: FeatureRuleOnetime
+  whitelist: FeatureRuleWhitelist
+}
+
+
+const useFeatureRules = ({ flagId } : Props) => {
+  const fetchFeatureRules = async (flagId: string | null) : Promise<QueryResult | null> => {
+    if (flagId == null) return null
+
+    const result = await apiService.getFeatureFlagRules(flagId)
+    const featureRuleDefault = result.find(featureRule => featureRule.type === 'DEFAULT') as FeatureRuleDefault
+    const featureRuleOnetime = result.find(featureRule => featureRule.type === 'ONETIME') as FeatureRuleOnetime
+    const featureRuleWhitelist = result.find(featureRule => featureRule.type === 'WHITELIST') as FeatureRuleWhitelist
+  
+    return {
+      default: featureRuleDefault,
+      onetime: featureRuleOnetime,
+      whitelist: featureRuleWhitelist
+    }
+  }
+
+  return useQuery(['featurerules', flagId], () => fetchFeatureRules(flagId))
+}
+
+export { useFeatureRules }
