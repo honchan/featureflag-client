44d10296e96779dfb1432f45c1c9259179e4559f
honchan <honkangchan@gmail.com>
2021-08-05 21:52:13 +0800

    update get featurerules to use react-query

--- src/featurerule/featurerule.component.tsx
+++ src/featurerule/featurerule.component.tsx
@@ -7,6 +7,7 @@ import { FeatureRuleDefault, FeatureRuleOnetime, FeatureRuleWhitelist} from '../
 import { DefaultFeatureRule } from './default-featurerule/default-featurerule.component'
 import { WhitelistFeatureRule } from './whitelist-featurerule/whitelist-featurerule.component'
 import { OnetimeFeatureRule } from './onetime-featurerule/featurerule.onetime.component'
+import { useFeatureRules } from '../querys/useFeatureRules'
 
 type Props = {
   activeFlag: string | null
@@ -15,34 +16,19 @@ type Props = {
 const { Panel } = Collapse
 
 const FeatureRule = ({ activeFlag } : Props) : JSX.Element => {
-  const [featureRuleDefault, setFeatureRuleDefault] = React.useState<FeatureRuleDefault | null>(null)
-  const [featureRuleOnetime, setFeatureRuleOnetime] = React.useState<FeatureRuleOnetime | null>(null)
-  const [featureRuleWhitelist, setFeatureRuleWhitelist] = React.useState<FeatureRuleWhitelist | null>(null)
-
-  React.useEffect(() => {
-    if (activeFlag == null) return
-  
-    const fetchFeatureRules = async () => {
-      const result = await apiService.getFeatureFlagRules(activeFlag)
-      setFeatureRuleDefault(result.find(featureRule => featureRule.type === 'DEFAULT') as FeatureRuleDefault)
-      setFeatureRuleOnetime(result.find(featureRule => featureRule.type === 'ONETIME') as FeatureRuleOnetime)
-      setFeatureRuleWhitelist(result.find(featureRule => featureRule.type === 'WHITELIST') as FeatureRuleWhitelist)      
-    }
-
-    fetchFeatureRules()
-  }, [activeFlag])
+  const { data  } = useFeatureRules({ flagId: activeFlag });
 
   return (
     <div className={classes.container}>
       <Collapse className={classes.collapse_group} defaultActiveKey="1">
-        <Panel key="1" header="Default Feature Rule" className={featureRuleDefault?.enabled ? classes.panelEnabled : classes.panelDisabled }>
-          <DefaultFeatureRule featureRule={featureRuleDefault} featureFlagId={activeFlag || ''} />
+        <Panel key="1" header="Default Feature Rule" className={data?.default?.enabled ? classes.panelEnabled : classes.panelDisabled }>
+          <DefaultFeatureRule featureRule={data?.default || null} featureFlagId={activeFlag || ''} />
         </Panel>
-        <Panel key="2" header="Whitelist Feature Rule" className={featureRuleWhitelist?.enabled ? classes.panelEnabled : classes.panelDisabled }>
-          <WhitelistFeatureRule featureRule={featureRuleWhitelist} />
+        <Panel key="2" header="Whitelist Feature Rule" className={data?.whitelist?.enabled ? classes.panelEnabled : classes.panelDisabled }>
+          <WhitelistFeatureRule featureRule={data?.whitelist || null} />
         </Panel>
-        <Panel key="3" header="Onetime Feature Rule" className={featureRuleOnetime?.enabled ? classes.panelEnabled : classes.panelDisabled }>
-          <OnetimeFeatureRule featureRule={featureRuleOnetime} />
+        <Panel key="3" header="Onetime Feature Rule" className={data?.onetime?.enabled ? classes.panelEnabled : classes.panelDisabled }>
+          <OnetimeFeatureRule featureRule={data?.onetime || null} />
         </Panel>
       </Collapse>
     </div>
