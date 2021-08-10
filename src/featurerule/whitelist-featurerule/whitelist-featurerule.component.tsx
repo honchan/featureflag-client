0e7a88f43bd0c641c8aec8ab3f2ebd8ab94d709f
honchan <honkangchan@gmail.com>
2021-08-10 19:57:25 +0800

    allow editing of whitelist featurerule

--- src/featurerule/whitelist-featurerule/whitelist-featurerule.component.tsx
+++ src/featurerule/whitelist-featurerule/whitelist-featurerule.component.tsx
@@ -1,45 +1,65 @@
 import * as React from 'react'
 import { Switch, Tag } from 'antd'
 
+import classes from './whitelist-featurerule.component.module.css'
 import { FeatureRuleWhitelist } from '../../api/api-types'
+import { useMutateWhitelist } from '../../querys/useMutateWhitelist'
+import { EditableTag } from '../../global/editable-tag/editable-tag.component'
 
 type Props = {
   featureRule: FeatureRuleWhitelist | null
+  featureFlagId: string
 }
 
-const WhitelistFeatureRule = ({ featureRule } : Props) : JSX.Element => {
-  const [onList, setOnList] = React.useState<string[]>(featureRule?.onList || [])
-  const [offList, setOffList] = React.useState<string[]>(featureRule?.offList || [])
+const WhitelistFeatureRule = ({ featureRule, featureFlagId } : Props) : JSX.Element => {
+  const { mutate } = useMutateWhitelist({ flagId: featureFlagId })
+  const onList = featureRule?.onList || []
+  const offList = featureRule?.offList || []
 
-  React.useEffect(() : void => {
-    if (featureRule) {
-      setOnList(featureRule.onList)
-      setOffList(featureRule.offList)
+  const handleRemoveFromList = (removedUser: string, list: "ON" | "OFF") => {
+    let onListCopy = onList
+    let offListCopy = offList
+
+    if (list === 'ON') {
+      onListCopy = onList.filter(user => user != removedUser)
+    } else {
+      offListCopy = offList.filter(user => user != removedUser)
     }
-  }, [featureRule])
-
-  const handleRemoveFromOnList = (removedUser : string) : void => {
-    setOnList(prevState => prevState.filter(user => user != removedUser))
+    mutate({ offList: offListCopy, onList: onListCopy, enabled: featureRule?.enabled || false})  
   }
 
-  const handleRemoveFromOffList = (removedUser : string) : void => {
-    setOffList(prevState => prevState.filter(user => user != removedUser))
-  }
+  const handleAddToList = (newTag: string, list: "ON" | "OFF") => {
+    let onListCopy = onList
+    let offListCopy = offList
 
-  console.log('onList', onList)
-  console.log('offList', offList)
+    if (list === 'ON') {
+      onListCopy = [...onListCopy, newTag]
+    } else {
+      offListCopy = [...offListCopy, newTag]
+    }
+    mutate({ offList: offListCopy, onList: onListCopy, enabled: featureRule?.enabled || false})  
+  }
 
   return (
     <>
-      <h3>Whitelist feature rule</h3>
-      <p>
-        <b>Enabled</b>
-        <Switch checked={featureRule?.enabled} />
-      </p>
-      <p>On List</p>
-      {onList.map(user => <Tag closable key={user} onClose={() => handleRemoveFromOnList(user)}>{user}</Tag>)}
-      <p>Off List</p>
-      {offList.map(user => <Tag closable key={user} onClose={() => handleRemoveFromOffList(user)}>{user}</Tag>)}
+      <div className={classes.status_container}>
+        <span className={classes.status_text}>Enabled</span>
+        <Switch checked={featureRule?.enabled} onChange={value => mutate({ onList, offList, enabled: value })} />
+      </div>
+      <div className={classes.tag_container}>
+        <p className={classes.tag_label}>On List</p>  
+        <div>
+          {onList.map(user => <Tag closable key={user} onClose={() => handleRemoveFromList(user, "ON")}>{user}</Tag>)}
+          <EditableTag displayText="Add New" onConfirm={(value) => handleAddToList(value, "ON")}/>
+        </div>
+      </div>
+      <div className={classes.tag_container}>
+        <p className={classes.tag_label}>Off List</p>  
+        <div>
+          {offList.map(user => <Tag closable key={user} onClose={() => handleRemoveFromList(user, "OFF")}>{user}</Tag>)}
+          <EditableTag displayText="Add New" onConfirm={(value) => handleAddToList(value, "OFF")}/>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/querys/useMutateWhitelist.tsx
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from 'react-query'
+import { apiService } from '../api/api-service'
+import { FEATURERULES_KEY } from '../querys/useFeatureRules'
+import { FeatureRuleWhitelistPayload } from '../api/api-types'
+
+type Props = {
+  flagId: string
+}
+
+const useMutateWhitelist = ({ flagId } : Props) => {
+  const queryClient = useQueryClient()
+
+  const updateWhitelistRule = async (flagId: string, payload: FeatureRuleWhitelistPayload) : Promise<void> => {
+    await apiService.updateWhitelistFeatureRule(flagId, payload)
+  }
+
+  return useMutation((payload: FeatureRuleWhitelistPayload) => updateWhitelistRule(flagId, payload), {
+    onSuccess: () => {
+      queryClient.invalidateQueries([FEATURERULES_KEY, flagId])
+    },  
+  })
+}
+
+export { useMutateWhitelist }
