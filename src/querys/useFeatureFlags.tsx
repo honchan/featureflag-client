e69b45a38ab17cc87a58e3382827c2b172833684
honchan <honkangchan@gmail.com>
2021-08-12 23:43:40 +0800

    use react-query for getting feature flags

---
+++ src/querys/useFeatureFlags.tsx
@@ -0,0 +1,18 @@
+import { useQuery } from 'react-query'
+
+import { apiService } from '../api/api-service'
+import { FeatureFlag } from '../api/api-types'
+
+const FEATUREFLAGS_KEY = 'featureflags'
+
+
+const useFeatureFlags = () => {
+  const fetchFeatureFlags = async () : Promise<FeatureFlag[]> => {
+    const result = await apiService.getFeatureFlags()
+    return result
+  }
+
+  return useQuery(FEATUREFLAGS_KEY, () => fetchFeatureFlags())
+}
+
+export { useFeatureFlags }
