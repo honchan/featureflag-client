286cfb0c4fc9a7f1c9e0a79bddf109bf8c8c92d2
honchan <honkangchan@gmail.com>
2021-08-13 22:06:53 +0800

    display flag name and description

--- src/featurerule/featurerule.component.tsx
+++ src/featurerule/featurerule.component.tsx
@@ -3,23 +3,35 @@ import { Collapse } from 'antd';
 
 import classes from './featurerule.component.module.css'
 import { apiService } from '../api/api-service'
-import { FeatureRuleDefault, FeatureRuleOnetime, FeatureRuleWhitelist} from '../api/api-types'
+import { FeatureFlag, FeatureRuleDefault, FeatureRuleOnetime, FeatureRuleWhitelist} from '../api/api-types'
 import { DefaultFeatureRule } from './default-featurerule/default-featurerule.component'
 import { WhitelistFeatureRule } from './whitelist-featurerule/whitelist-featurerule.component'
 import { OnetimeFeatureRule } from './onetime-featurerule/onetime-featurerule.component'
 import { useFeatureRules } from '../querys/useFeatureRules'
+import { useFeatureFlags } from '../querys/useFeatureFlags'
+
 
 type Props = {
-  activeFlag: string | null
+  activeFlag: string
 }
 
 const { Panel } = Collapse
 
 const FeatureRule = ({ activeFlag } : Props) : JSX.Element => {
   const { data  } = useFeatureRules({ flagId: activeFlag });
+  const { data: flags = [] } = useFeatureFlags()
+
+  const filterForActiveFlag = (flagsList : FeatureFlag[], flagId: number) => {
+    return flagsList.find(flag => flag.id === flagId)
+  }
+
+  const flag = filterForActiveFlag(flags, parseInt(activeFlag))
+
 
   return (
     <div className={classes.container}>
+      <h1>{flag?.name}</h1>
+      <p>{flag?.description}</p>
       <Collapse className={classes.collapse_group} defaultActiveKey="1">
         <Panel key="1" header="Default Feature Rule" className={data?.default?.enabled ? classes.panelEnabled : classes.panelDisabled }>
           <DefaultFeatureRule featureRule={data?.default || null} featureFlagId={activeFlag || ''} />
