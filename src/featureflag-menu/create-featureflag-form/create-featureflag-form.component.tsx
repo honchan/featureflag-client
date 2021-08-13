2e2c574ff59fff0b6bef48c849425120971ea5b1
honchan <honkangchan@gmail.com>
2021-08-13 22:07:31 +0800

    create feature flag form

---
+++ src/featureflag-menu/create-featureflag-form/create-featureflag-form.component.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react'
+import { Input, Collapse, Button } from 'antd'
+
+import { useMutateCreateFeatureflag } from '../../querys/useMutateCreateFeatureflag'
+import classes from './create-featureflag-form.component.module.css'
+
+const { Panel } = Collapse
+
+const CreateFeatureflagForm = () : JSX.Element => {
+  const [flagname, setFlagname] = React.useState<string>('')
+  const [description, setDescription] = React.useState<string>('')
+
+  const onComplete = () => {
+    setFlagname('')
+    setDescription('')
+  }
+
+  const { mutate, isLoading } = useMutateCreateFeatureflag({ onComplete })
+
+  const handleInputChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name
+    if (name === 'flagname') {
+      setFlagname(e.target.value)
+    } else if (name === 'description') {
+      setDescription(e.target.value)
+    }
+  }
+
+  const handleSubmit = async () : Promise<void> => {
+    mutate({ newFlag: flagname, description })
+  }
+
+  const disabled = flagname === ''
+
+  return (
+    <Collapse>
+      <Panel key="create-featureflag-panel" header="Create a new featureflag" className={classes.background_color}>
+        <div className={classes.create_flag_form}>
+          <Input size="large" placeholder="Featureflag name" value={flagname} onChange={handleInputChange} name="flagname"/>
+          <Input placeholder="Description" value={description} onChange={handleInputChange} name="description"/>
+          <Button type="primary" loading={isLoading} disabled={disabled} onClick={handleSubmit}>Submit</Button>
+        </div>
+      </Panel>
+    </Collapse>
+  )
+}
+
+export { CreateFeatureflagForm }
